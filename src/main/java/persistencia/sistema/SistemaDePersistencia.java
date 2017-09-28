6bf15fee171d65f5fa5ca506d109637cbafc8eab
Andrés Carrasquilla <kehuboandres@gmail.com>
2017-09-28 13:38:39 -0500

    Modificacion al Gradle

--- src/main/java/persistencia/sistema/SistemaDePersistencia.java
+++ src/main/java/persistencia/sistema/SistemaDePersistencia.java
@@ -32,6 +32,6 @@ public class SistemaDePersistencia {
 	}
 
 	public EntityManager getEntityManager() { 
-		return entityManager;   //    ------------ Creado por Andrés --------------------------------
+		return entityManager;   
 	}
 }
